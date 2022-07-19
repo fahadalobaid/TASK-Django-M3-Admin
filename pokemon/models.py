538fe7336e87d1b29ecfbb094a042294b8bdd0ca
fahad alobaid <fahad.n.b.alobaid@gmail.com>
2022-07-19 16:55:48 +0300

    end first part of the task

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,26 @@
+from time import timezone
 from django.db import models
+class Pokemon (models.Model):
+    
+
+    name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.name
+        
+    class  PokemonType (models.TextChoices):
+        WATER = 'WA', ('WATER')
+        GRASS = 'GR', ('GRASS')
+        GHOST = 'GH', ('GHOST')
+        STEEL = 'ST', ('STEEL')
+        FAIRY = 'FA', ('FAIRY')
+    type = models.CharField(max_length=2,choices=PokemonType.choices,default=PokemonType.WATER)
+    hp           = models.PositiveBigIntegerField()     
+    active       = models.BooleanField(default=True)   
+    name_fr      = models.CharField(max_length=30,default="")   
+    name_ar      = models.CharField(max_length=30,default="")   
+    name_jp      = models.CharField(max_length=30,default="")     
+    created_at   = models.DateTimeField(auto_created=True)    
+    modified_at  = models.DateField(auto_now_add=True)
+
 
 # Create your models here.
